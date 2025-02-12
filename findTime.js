85c5685dfd36329d146c75135ddfadaa41362523
Amine-Miadi <A.miadi@outlook.com>
2025-02-12 20:39:05 +0100

    working version

--- findTime.js
+++
@@ -1,3 +0,0 @@
-export default function findDepartureAtTime(arr, time) {
-    return arr.find(obj => obj.dateTimeDepart.includes(time));
-}
--- src/getDates.js
+++ src/getDates.js
@@ -2,17 +2,20 @@ const getNextWeekDates = (cur) => {
   const today = new Date();
   // Get the current day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
   const currentDay = today.getDay();
+  console.log(currentDay);
   
   
 
   // Calculate how many days to add to get to the next Monday
-  const daysUntilNextMonday = (currentDay === 0 ? 1 : 7 - currentDay);
+  const daysUntilNextMonday = (currentDay === 0 ? 0 : 7 - currentDay);
+  console.log(daysUntilNextMonday);
 
   // Start from the next Monday and loop to get Monday to Friday
   const nextWeekDates = [];
   for (let i = 1; i <= 5; i++) {
       const nextDay = new Date(today);
       nextDay.setDate(today.getDate() + daysUntilNextMonday + i);
+      console.log(nextDay);
       nextWeekDates.push(nextDay.toISOString().split('T')[0]); // Format to YYYY-MM-DD
   }
 
